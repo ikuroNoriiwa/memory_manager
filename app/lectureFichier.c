e22614bcadd37bab133ec9a79e3d974230192777
IkuroNoriiwa <math-and-rowing@hotmail.fr>
2020-06-12 22:13:46 +0200

    :computer: lecture des fichier - lecture memoire

---
+++ app/lectureFichier.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../util/util.h"
+
+
+int getMeminfo(){
+	FILE *fichier = fopen("/proc/meminfo","r");
+	char ligne[200];
+	long memTotale = 0;
+
+	while(fgets(ligne, sizeof ligne, fichier) != NULL){
+		if(strstr(ligne, "MemTotal:") != NULL){
+			char *ptr = ligne;
+			printf("%s", ligne); 
+			memTotale = fnc_getNumberInString(ligne);
+			printf("%d\n", memTotale+1);
+			printf("Gig %.2f\n", fnc_nBtoGB(memTotale));
+		}
+	}
+	fclose(fichier);
+
+	return 0; 
+} 
