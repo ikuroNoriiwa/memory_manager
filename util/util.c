2a005d6d961c41f527b224e2ad470ae597d3a1cf
ikuroNoriiwa <math-and-rowing@hotmail.fr>
2020-06-12 14:52:15 +0200

    :computer: ajout module util (dev)

---
+++ util/util.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <ctype.h>
+
+/**
+ * converti les ko(KiloOctets ou KiloByte en anglais) en Go (GigaOctet ou GigaByte en anglais)
+ * @param taille : int correspondant à la taille en ko
+ * @return float : la conversion en Go 
+ */
+float fnc_kBtoGB(int taille){
+	return (taille / pow(10,6));
+}
+
+/**
+ * converti une string (char*) en int 
+ * supprime les lettres et ne récupère que les chiffre
+ * @param str : char* comprenant lettre en chiffre (ex type de RAM et taille)
+ * @return int : int récupéré dans la string 
+ */
+int fnc_getNumberInString(char* str){
+	char *ptr = str;
+	int ret = 0;
+	while(*ptr){
+		if(isdigit(*ptr)){
+			ret = strtol(ptr, &ptr, 10);
+		}else{
+			ptr++;
+		}
+	}
+
+	return ret; 
+}
