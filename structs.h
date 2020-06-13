16de042113be5dca36ca8bbe410d2d4030b4a24a
IkuroNoriiwa <math-and-rowing@hotmail.fr>
2020-06-13 16:40:56 +0200

    :floppy_disk: Ajout structure DefProcessus

--- structs.h
+++ structs.h
@@ -9,5 +9,16 @@
 		int cached;
 		int buffers; 
 	}DefMemory; 
-
+	
+	typedef struct{
+		char* user; 	//Nom d'utilisateur propriétaire du processus
+		int pid; 	//PID 
+		int ppid;	//PID du parent
+		float cpu;	// CPU consommé -À TROUVER-
+		float mem;	// mémoire consommé -À TROUVER-
+		char* status;	// état du processus (RSDZTW) 
+		char* command;	// commande lancée
+		int start;	// heure de lancement
+		int time; 	// temps de fonctionnement 
+	}
 #endif
