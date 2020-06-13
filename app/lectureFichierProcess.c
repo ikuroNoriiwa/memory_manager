379c2562617a34696e426906de916f1e36c7fd1b
IkuroNoriiwa <math-and-rowing@hotmail.fr>
2020-06-13 19:41:39 +0200

    :computer: modification fichier lecture processus + add getUser

--- app/lectureFichierProcess.c
+++ app/lectureFichierProcess.c
@@ -1,16 +1,11 @@
-#include <stdlib.h>
-#include <stdio.h> 
-#include <ctype.h>
-#include <string.h>
-#include <dirent.h>
-
-#include "../util/util.h"
+#include "lectureFichierProcess.h"
 
 
-char* getProcessName(char* pidProcess){
+DefProcessus* getProcessName(char* pidProcess){
 	char path[25] = "/proc/"; 
 	int i = 0;
-
+	
+	DefProcessus* processus = (DefProcessus*)malloc(sizeof(DefProcessus));
 	strcat(path,pidProcess);
 	strcat(path,"/status");
 	
@@ -18,20 +13,34 @@ char* getProcessName(char* pidProcess){
 	char ligne[200];
 
 	while(fgets(ligne, sizeof ligne, fichier) != NULL){
+		//Get name from ps>
+		//printf("debug : %s",ligne);
 		if(strstr(ligne, "Name:	") != NULL){
 			for(i = 0; i < 6; i++){
 				fnc_removeChar(ligne, 1);
 			}
-			printf("PRocNAme - NAME : %s",ligne);
-			//printf(" + len %d et %c\n", strlen(ligne), ligne[6]);
-
+			processus->name = (char*)malloc(sizeof(char)*sizeof ligne);
+			strcpy(processus->name,ligne);
+			processus->name[strlen(processus->name)-1] = '\0';
+			//printf("PRocNAme - NAME : %s",processus->name);
+			//printf(" + len %d et %c", strlen(ligne), ligne[6]);
+		}else if(strstr(ligne, "Uid:") != NULL){
+			//printf(" et %s \n",getUser(fnc_getNumberInString(ligne)));
+			processus->user = getUser(fnc_getNumberInString(ligne));
+		}else if((strstr(ligne, "Pid:") != NULL) && (strstr(ligne,"Tracer") == NULL)){
+			if(strstr(ligne, "PPid:")!= NULL){
+				processus->ppid = fnc_getNumberInString(ligne);
+				printf("et ppid:%d\n", processus->ppid);
+			}else{
+				processus->pid = fnc_getNumberInString(ligne);
+				printf(" ET pid:%d \n", processus->pid);
+			}
 		}
+	//	printf("\n");
 	}
 	
 	fclose(fichier);	
 	
-	
-	return NULL;
 }
 
 void test(){
@@ -55,3 +64,9 @@ void test(){
 	}
 }
 
+char* getUser(uid_t uid){
+	struct passwd *pws; 
+	pws = getpwuid(uid);
+
+	return pws->pw_name;
+}
--- app/lectureFichierProcess.h
+++ app/lectureFichierProcess.h
@@ -2,6 +2,18 @@
 #ifndef _LECTUREFICHIERPROCESS_H
 #define _LECTUREFICHIERPROCESS_H
 
-	char* getProcessName(char* pidProcess);
+#include <sys/types.h>
+#include <pwd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h> 
+#include <string.h>
+#include <dirent.h>
+
+#include "../structs.h"
+#include "../util/util.h"
+
+	DefProcessus* getProcessName(char* pidProcess);
 	void test();
+	char* getUser(uid_t uid);
 #endif 
